671e3033453a515d29efb0707965c38ce0eadb4e
unknown <Bhavit Kanthalia>
2020-11-08 09:35:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Quotes from './components/Quotes';
+import { Navbar , Container , NavbarBrand } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar id="navbar" expand="lg" >
+        <Container>
+        <Navbar.Brand href="#">Motivate-me</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Quotes/>
+
+      <div className="fixed-bottom">  
+            <Navbar color="dark" dark>
+                <Container>
+                    <NavbarBrand id="footer">
+                      <p id="p-footer">
+                      Developed by BK
+                      </p>
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+        </div>
     </div>
   );
 }
